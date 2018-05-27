bf2c3088efacec4fa3850dc488afc5f54ec9e917
Tramaine Gillus <tramaine.gillus@gmail.com>
2018-05-27 01:31:09 -0400

    Initial commit.

---
+++ playground/mongodb-find.js
@@ -0,0 +1,52 @@
+// const MongoClient = require("mongodb").MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+  if (err) {
+    return console.log("Unable to connect to MongoDB server");
+  }
+
+  console.log("Connected to MongoDB server");
+
+  // db
+  //   .collection("Todos")
+  //   .find({ _id: new ObjectID("5b03995b0adaef30812a4f0a") })
+  //   .toArray()
+  //   .then(
+  //     docs => {
+  //       console.log("Todos");
+  //       console.log(JSON.stringify(docs, undefined, 2));
+  //     },
+  //     err => {
+  //       console.log("Unable to fetch todos", err);
+  //     }
+  //   );
+  db
+    .collection("Todos")
+    .find()
+    .count()
+    .then(
+      count => {
+        console.log(`Todos count: ${count}`);
+      },
+      err => {
+        console.log("Unable to fetch todos", err);
+      }
+    );
+
+  db
+    .collection("Users")
+    .find({ name: "Tramaine" })
+    .toArray()
+    .then(
+      docs => {
+        console.log("Users:");
+        console.log(JSON.stringify(docs, undefined, 2));
+      },
+      err => {
+        console.log("Unable to fettch users");
+      }
+    );
+
+  // db.close();
+});
