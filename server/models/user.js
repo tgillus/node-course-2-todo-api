6c2218c1b8b778f8a37c29d0326ef39179ddd312
Tramaine Gillus <tramaine.gillus@gmail.com>
2018-11-05 13:32:04 -0500

    Add POST /users/login route.

--- server/models/user.js
+++ server/models/user.js
@@ -76,6 +76,28 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  const User = this;
+
+  return User.findOne({
+    email
+  }).then(user => {
+    if (!user) {
+      return Promise.reject("User not found.");
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject("Invalid credentials.");
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre("save", function(next) {
   const user = this;
 
