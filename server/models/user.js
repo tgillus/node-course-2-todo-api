34eb7caaccb4eac370ebd6961ab57a71495a826d
Tramaine Gillus <tramaine.gillus@gmail.com>
2018-11-05 19:19:17 -0500

    Add DELETE /users/me/token route.

--- server/models/user.js
+++ server/models/user.js
@@ -59,6 +59,16 @@ UserSchema.methods.generateAuthToken = function() {
   return user.save().then(() => token);
 };
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this;
+
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token) {
   const User = this;
   let decoded;
--- server/server.js
+++ server/server.js
@@ -131,6 +131,15 @@ app.post("/users/login", (req, res) => {
     .catch(err => res.status(400).send(err));
 });
 
+app.delete("/users/me/token", authenticate, (req, res) => {
+  req.user
+    .removeToken(req.token)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch(err => res.status(400).send());
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}.`);
 });
