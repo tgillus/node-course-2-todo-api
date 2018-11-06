83763c39caeaf8e8596517065569f1a4cb6afc27
Tramaine Gillus <tramaine.gillus@gmail.com>
2018-11-05 19:34:19 -0500

    Add tests for DELETE /users/me/token.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -294,7 +294,6 @@ describe("POST /users/login", () => {
         User.findById(users[1]._id)
           .then(user => {
             expect(user.tokens.length).toBe(0);
-
             done();
           })
           .catch(err => done(err));
@@ -313,5 +312,27 @@ describe("POST /users/login", () => {
         expect(res.body).toEqual({ error: "User not found." });
       })
       .end(done);
+  });
+});
+
+describe("DELETE /users/me/token", () => {
+  it("should remove auth token on logout", done => {
+    request(app)
+      .delete("/users/me/token")
+      .set("x-auth", users[0].tokens[0].token)
+      .send()
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(err => done(err));
+      });
   });
 });
