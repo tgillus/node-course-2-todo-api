a9e3edcfdd844393b30c56446e18fa41043c2ba1
Tramaine Gillus <tramaine.gillus@gmail.com>
2018-12-09 20:57:55 -0500

    Fix typo.

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -44,7 +44,7 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
         console.log(JSON.stringify(docs, undefined, 2));
       },
       err => {
-        console.log("Unable to fettch users");
+        console.log("Unable to fetch users");
       }
     );
 
